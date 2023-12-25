f3aee242f975a46c7feb1ea02c997b36dbb3466c
Allen Goodman <goodmaa3@gene.com>
2023-12-25 16:15:10 -0500

    Yeji

--- docs/conf.py
+++ docs/conf.py
@@ -1,26 +1,15 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
-project = "Yeji"
-copyright = "2023, Allen Goodman"
 author = "Allen Goodman"
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+copyright = "2023 Genentech, Inc."
+
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 extensions = []
 
-templates_path = ["_templates"]
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
-
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+html_static_path = ["_static"]
 
 html_theme = "alabaster"
-html_static_path = ["_static"]
+
+project = "Yeji"
+
+templates_path = ["_templates"]
