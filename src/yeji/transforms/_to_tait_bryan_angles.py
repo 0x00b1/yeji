08f92778491f3497f5386971e5d93f6377c968ec
Allen Goodman <goodmaa3@gene.com>
2024-01-01 17:59:03 -0500

    Yeji

---
+++ src/yeji/transforms/_to_tait_bryan_angles.py
@@ -0,0 +1,18 @@
+from typing import Dict, Any
+
+from torch import Tensor
+
+import yeji.transforms.functional
+from yeji.features import EulerAngles
+from ._transform import Transform
+
+
+class ToTaitBryanAngles(Transform):
+    _transformed_types = (Tensor,)
+
+    def _transform(
+        self,
+        input: Tensor,
+        parameters: Dict[str, Any],
+    ) -> EulerAngles:
+        return yeji.transforms.functional.to_tait_bryan_angles(input)
