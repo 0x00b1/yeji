5e391913d3e304740340f6992dacd09d014c4a16
Allen Goodman <goodmaa3@gene.com>
2024-02-16 16:12:51 -0500

    Yeji

--- src/yeji/transforms/__init__.py
+++ src/yeji/transforms/__init__.py
@@ -1,11 +1,11 @@
 from ._compose import Compose
 from ._identity import Identity
 from ._lambda import Lambda
-from ._to_euler_angles import ToEulerAngles
+from ._to_euler_angle import ToEulerAngles
 from ._to_rotation_matrix import ToRotationMatrix
-from ._to_rotation_quaternions import ToRotationQuaternions
+from ._to_rotation_quaternion import ToRotationQuaternions
 from ._to_rotation_vector import ToRotationVector
-from ._to_tait_bryan_angles import ToTaitBryanAngles
+from ._to_tait_bryan_angle import ToTaitBryanAngles
 from ._to_tensor import ToTensor
 from ._transform import Transform
 
