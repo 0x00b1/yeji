3d1f9a74dbc6f1b29b24aec127705b3dab356dc2
Allen Goodman <goodmaa3@gene.com>
2024-02-16 16:20:03 -0500

    Yeji

--- src/yeji/features/_tait_bryan_angle.py
+++ src/yeji/features/_tait_bryan_angle.py
@@ -9,14 +9,40 @@ from ._feature import Feature
 
 
 class TaitBryanAngle(Feature):
+    axes: str
+    degrees: bool
+
     @classmethod
-    def _wrap(cls, tensor: Tensor) -> TaitBryanAngle:
-        return tensor.as_subclass(cls)
+    def _wrap(
+        cls,
+        tensor: Tensor,
+        *,
+        axes: str = "xyz",
+        degrees: bool = False,
+    ) -> TaitBryanAngle:
+        if tensor.ndim == 0:
+            raise ValueError
+
+        if tensor.shape[-1] != 3:
+            raise ValueError
+
+        if tensor.ndim == 1:
+            tensor = torch.unsqueeze(tensor, 0)
+
+        tait_bryan_angle = tensor.as_subclass(cls)
+
+        tait_bryan_angle.axes = axes
+
+        tait_bryan_angle.degrees = degrees
+
+        return tait_bryan_angle
 
     def __new__(
         cls,
         data: Any,
         *,
+        axes: str = "xyz",
+        degrees: bool = False,
         dtype: Optional[torch.dtype] = None,
         device: Optional[Union[torch.device, str, int]] = None,
         requires_grad: Optional[bool] = None,
@@ -28,27 +54,10 @@ class TaitBryanAngle(Feature):
             requires_grad=requires_grad,
         )
 
-        if tensor.ndim == 0:
-            raise ValueError
-
-        if tensor.shape[-1] != 3:
-            raise ValueError
-
-        if tensor.ndim == 1:
-            tensor = torch.unsqueeze(tensor, 0)
-
-        return cls._wrap(tensor)
-
-    @classmethod
-    def wrap_like(
-        cls,
-        other: TaitBryanAngle,
-        tensor: Tensor,
-    ) -> TaitBryanAngle:
-        return cls._wrap(tensor)
+        return cls._wrap(tensor, axes=axes, degrees=degrees)
 
     def __repr__(self, *, tensor_contents: Any = None) -> str:
-        return self._make_repr()
+        return self._make_repr(axes=self.axes, degrees=self.degrees)
 
 
 _TaitBryanAngleType = Union[Tensor, TaitBryanAngle]
