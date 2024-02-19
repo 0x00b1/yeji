65bd7c82b0366e13a813bab95eb72d43caad45ca
Allen Goodman <goodmaa3@gene.com>
2024-02-19 12:12:41 -0500

    Yeji

--- docs/conf.py
+++ docs/conf.py
@@ -2,12 +2,11 @@ author = "Allen Goodman"
 
 autoapi_dirs = ["../src/yeji"]
 
-copyright = "2023 Genentech, Inc"
+copyright = "2024 Genentech, Inc"
 
 exclude_patterns = [".DS_Store", "Thumbs.db", "_build"]
 
 extensions = [
-    "autoapi.extension",
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
     "sphinx_rtd_theme",
