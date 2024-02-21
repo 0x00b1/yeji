3dfaaf9fd19a2d555f8824d5ab18db92d8c9efb7
Allen Goodman <goodmaa3@gene.com>
2024-02-21 16:23:56 -0500

    Yeji

--- src/yeji/datasets/_aqsoldb_solubility_dataset.py
+++ src/yeji/datasets/_aqsoldb_solubility_dataset.py
@@ -36,7 +36,7 @@ class AqSolDBSolubilityDataset(_TDCDataset):
             root=root,
             download=download,
             identifier="https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/OVHAW8/RLVXZU",
-            name="curated-solubility-dataset.tab",
+            suffix="curated-solubility-dataset.tab",
             checksum="5370aa67615adb2f11806ed1aaed37c2bf91e634d36ebaf40509c16d5cede8a0",
             x_columns=["SMILES", "Solubility"],
         )
--- src/yeji/datasets/_disgenet_dataset.py
+++ src/yeji/datasets/_disgenet_dataset.py
@@ -36,7 +36,7 @@ class DisGeNETDataset(_TDCDataset):
             root=root,
             download=download,
             identifier=4168282,
-            name="disgenet.csv",
+            suffix="disgenet.csv",
             checksum="md5:b7efdf1dc006ff04a33bb3a4aec5d746",
             x_columns=["X1", "ID2"],
             y_columns=["Y"],
